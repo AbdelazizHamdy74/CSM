365391cb70848ef5dd96f95a4a9e88a6155e2ab9
Abdelaziz Hamdy <abdelazizhamdy74@gmail.com>
2024-12-08 20:09:32 +0200

    add, update, get asset

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
 const express = require("express");
+const cors = require("cors");
+const path = require("path");
 
 const app = express();
 
+// Middleware to parse JSON requests
 app.use(express.json());
+
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
+// إعداد CORS
+const corsOptions = {
+  origin: "http://localhost:4200",
+  methods: ["GET", "PUT", "POST"],
+  credentials: true,
+};
+
+// استخدام CORS
+app.use(cors(corsOptions));
+
 // Import Routes
 const assetRoutes = require("./src/asset/assetRoute");
 
@@ -11,5 +27,5 @@ app.use("/api/assets", assetRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`App running running on port ${PORT}`);
+  console.log(`App running on port ${PORT}`);
 });
